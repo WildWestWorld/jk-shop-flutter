e8d114e642dab1a8f87a4ecfd4ccaccead29069b
JOTARO <380686356@qq.com>
2023-05-21 10:13:53 +0800

    主题引入字体

--- lib/common/style/theme.dart
+++ lib/common/style/theme.dart
@@ -1,6 +1,5 @@
 // 主题色
 
-
 import 'package:flutter/material.dart';
 
 import 'index.dart';
@@ -10,10 +9,12 @@ class AppTheme {
   /// 亮色
   static ThemeData light = ThemeData(
     colorScheme: lightColorScheme,
+    fontFamily: "Montserrat",
   );
 
   /// 暗色
   static ThemeData dark = ThemeData(
     colorScheme: darkColorScheme,
+    fontFamily: "Montserrat",
   );
 }

65b5ee6adad870a3b7dba25fa95261560e5f606f
JOTARO <380686356@qq.com>
2023-06-07 20:40:26 +0800

    商品详情 轮播图

--- lib/common/api/product.dart
+++ lib/common/api/product.dart
@@ -30,4 +30,12 @@ class ProductApi {
     }
     return products;
   }
+
+  /// 商品详情
+  static Future<ProductModel> productDetail(int? id) async {
+    var res = await ApiOrginService.to.get(
+      '/products/$id',
+    );
+    return ProductModel.fromJson(res.data);
+  }
 }
